c04b81db43d1127bc190a82a8116178ed408da89
lrezo <omran.00@hotmail.it>
2025-11-18 19:24:21 +0100

    Homepage styled and directory and file input created

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { NgModel } from '@angular/forms';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonText,
+  IonItem,
+  IonLabel,
+  IonSelect,
+  IonSelectOption,
+  IonInput,
+  IonButton,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonButton,
+    IonInput,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonText,
+    IonItem,
+    IonLabel,
+    IonSelect,
+    IonSelectOption,
+  ],
 })
 export class HomePage {
+  directory!: string;
+  fileBaseName!: string;
   constructor() {}
+
+  selectedFolder(event: CustomEvent) {
+    this.directory = event.detail.value;
+  }
+  filenameInputChanged(event: CustomEvent) {
+    console.log(event.detail.value);
+    console.log(this.fileBaseName);
+  }
 }
