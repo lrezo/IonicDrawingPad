f4904a68de70cd6794401e239736a4e992a7a962
lrezo <omran.00@hotmail.it>
2025-11-18 17:38:23 +0100

    created a drawing service where i can retrieve always the filepath destination with saveImage function

--- src/app/drawing-service.ts
+++ src/app/drawing-service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
-
+import { Filesystem, Directory } from '@capacitor/filesystem';
 @Injectable({
   providedIn: 'root',
 })
 export class DrawingService {
-  
+  directory: Directory = Directory.Documents;
+  fileBaseName: String = `drawing`;
+
+  setDestination(dir: Directory, fileName: string) {
+    this.directory = dir;
+    this.fileBaseName = fileName;
+  }
+  getDestination() {
+    return {
+      directory: this.directory,
+      fileBaseName: this.fileBaseName,
+    };
+  }
+
+  async SaveDrawing(imageBase64Png: string): Promise<void> {
+    const timeStamp = new Date().getTime();
+    const filename = `${this.fileBaseName}_${timeStamp}.png`;
+
+    await Filesystem.writeFile({
+      path: filename,
+      directory: this.directory,
+      data: imageBase64Png,
+    });
+  }
 }
